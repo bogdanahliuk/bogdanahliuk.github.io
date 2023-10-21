16f2b2fd206a4f07e3c51a3898a933bbfae15bd0
bogdanahliuk <115418105+bogdanahliuk@users.noreply.github.com>
2023-10-21 15:31:22 +0100

    LR2

---
+++ LR2/script.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('change_btn');
+let titles = document.getElementsByTagName('h2');
+let paragraphs = document.getElementsByTagName('p');
+let cells = document.getElementsByTagName('td');
+
+button.addEventListener('click', () => {
+    for(let i = 6; i < 9; i++){
+        cells[i].innerHTML = '';
+        cells[i].appendChild(document.getElementById(i));
+    }
+    for(let i = 3; i < 6; i++){
+        cells[i].innerHTML = '';
+        cells[i].appendChild(document.getElementById(i));
+    }
+})
