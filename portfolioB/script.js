7aef511ee073610e52fa313ea991c9d4eb32292b
bogdanahliuk <115418105+bogdanahliuk@users.noreply.github.com>
2023-11-28 14:13:29 +0000

    portfolio update

--- portfolioB/script.js
+++ portfolioB/script.js
@@ -1,8 +1,17 @@
 function myFunction() {
-	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
-	}
+  let x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+let list_boxes = document.querySelectorAll(".list_box");
+let list_btns = document.querySelectorAll(".list_btn");
+list_btns.forEach((element) => {
+  element.addEventListener("click", () => {
+    let btn_id = element.id.slice(-1);
+    list_boxes[btn_id - 1].classList.toggle("open");
+  });
+});
